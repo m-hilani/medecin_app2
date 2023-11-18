932a1ad8ad4ec5712edbc68d90f4fe923ea23126
m-hilani <148714322+m-hilani@users.noreply.github.com>
2023-11-18 10:19:29 +0300

    home page added

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -5,6 +5,70 @@ class HomePage extends StatelessWidget {
   static String id = "HomePage";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Container(
+            //   height: 60.0,
+            //   child: Image.asset('images/logo.png'),
+            // ),
+            const Text(
+              textAlign: TextAlign.center,
+              'Medicine WareHouse',
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 45.0,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+
+            const SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to login screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to registration screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: const Text(
+                    'Register',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
